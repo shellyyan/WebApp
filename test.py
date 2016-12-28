2c383d2624243705a403af8a5bcb50c96e99e2d0
shellyyan <274099220@qq.com>
2016-12-28 19:34:27 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,21 @@
-import orm
-import asyncio
-from models import User,Blog,Comment
+import asyncio,aiomysql
+from models import User
 @asyncio.coroutine
-def test():
-    yield from orm.create_pool(loop,host='127.0.0.1',user='www-data',password='www-data',database='awesome')
-    u=User(name='Test4',email='test4@example.com',passwd='1234567890',image='about:blank')
-    yield from u.save()
+def test():  #这里使用aiomysql文档的示例写法，直接执行insert语句方式插入数据
+    pool = yield from aiomysql.create_pool(host='localhost', port=3306,
+              user='www-data', password='www-data',
+              db='awesome', maxsize=10,minsize=1,loop=loop)
+    with (yield from  pool) as conn:
+        cursor=yield from conn.cursor()
+        user = User(id='1',name='Administer',admin='1',email='admin@example.com',passwd='123456',image='about:blank',created_at='20161211')
+        args = list(map(user.getValueOrDefault, user.__fields__))
+        args.append(user.getValueOrDefault(user.__primary_key__))
+        yield from cursor.execute(user.__insert__.replace('?','%s') , args )
+        yield from cursor.close()
+        yield from conn.commit()
+    pool.close()
     yield from pool.wait_closed()
-loop=asyncio.get_event_loop()
+
+loop = asyncio.get_event_loop()
 loop.run_until_complete(test())
-loop.run_forever()
+loop.close()
